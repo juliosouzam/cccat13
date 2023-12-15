44052dbf00a32c82eb038c4476c440f751b441ae
Júlio César <julio.souzam@gmail.com>
2023-12-15 20:54:14 -0300

    feat(payments): add delay to process

--- backend/payments/src/application/usecases/ProcessPayment.ts
+++ backend/payments/src/application/usecases/ProcessPayment.ts
@@ -1,9 +1,15 @@
 import { Queue } from '../../infra/queue/Queue';
 
+const sleep = async (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
 export class ProcessPayment {
   constructor(private readonly queue: Queue) {}
 
   async execute(input: any): Promise<void> {
+    await sleep(2000);
     console.log('ProcessPayment', input);
 
     await this.queue.publish('payment.approved', {
